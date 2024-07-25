a7539b371901472750604eac269edf19652c9199
Himanshu Kesharwani <himanshu.kesharwani.02@gmail.com>
2024-07-25 15:36:24 +0530

    [ADD]: Initial commit.

---
+++ SwiftUI-Food-App/SwiftUI-Food-App/SwiftUI_Food_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Food_AppApp.swift
+//  SwiftUI-Food-App
+//
+//  Created by Himanshu Kesharwani on 25/07/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Food_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
